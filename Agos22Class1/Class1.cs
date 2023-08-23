d2e1fd9680428b36971f80d9b7b8ddfdaeda5076
Braian-Ramirez <braiannf.ramirezo@konradlorenz.edu.co>
2023-08-22 23:16:28 -0500

    Culminación
    Se agregaron los dos ejercicios faltantes.

--- Agos22Class1/Class1.cs
+++ Agos22Class1/Class1.cs
@@ -8,5 +8,39 @@ namespace Agos22Class1
 {
     internal class Class1
     {
+        public static void ExecuteExecersice1()
+        {
+            Console.WriteLine("Ingrese tres números enteros:");
+                int a, b, c, ResultadoSuma, ResultadoPromedio;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            ResultadoSuma = a + b + c;
+            ResultadoPromedio = ResultadoSuma / 3;
+            Console.WriteLine("La suma resultante es: " + ResultadoSuma);    
+            Console.WriteLine("El promedio de los números ingresados es: " +  ResultadoPromedio);
+        }
+        public static void ExecuteExecersice2()
+        {
+            Console.WriteLine("Ingrese la base y la altura del rectangulo:");
+            int altura, cimiento, perimetro, area;
+            cimiento = int.Parse(Console.ReadLine());
+            altura = int.Parse(Console.ReadLine());
+            perimetro = (cimiento * 2) + (altura * 2);
+            area = cimiento * altura;
+            Console.WriteLine("El área del rectangulo es: " +  area + " El perimetro del rectangulo es: " + perimetro);
+        }
+        public static void ExecuteExecersice3()
+        {
+            Console.WriteLine("Ingrese el número total de hombres y de mujeres que hay en su grupo: ");
+            int hombres, mujeres, poblacion; 
+            int porcentajeHombres, porcentajeMujeres;
+            hombres = int.Parse(Console.ReadLine());
+            mujeres = int.Parse(Console.ReadLine());
+            poblacion = hombres + mujeres;
+            porcentajeHombres = (hombres * 100) / poblacion ;
+            porcentajeMujeres = mujeres * 100 / poblacion ;
+            Console.WriteLine("El porcentaje de hombres es de: %" + porcentajeHombres + " El porcentaje de mujeres es: %" +  porcentajeMujeres);
+        } 
     }
 }
--- Agos22Class1/Program.cs
+++ Agos22Class1/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Seleccione el ejercicio que desea ejecutar:\n 1. Suma de tres enteros y su promedio\n 2. Área y perimetro de un rectángulo\n 3. Porcentaje población\n");
+            int op;
+            op = int.Parse(Console.ReadLine());
+            switch (op) {
+                case 1: 
+                    Console.WriteLine("Promedio");
+                    Class1.ExecuteExecersice1();
+                    break;
+                case 2:
+                    Console.WriteLine("Área y perimetro");
+                    Class1.ExecuteExecersice2();
+                    break;
+                case 3:
+                    Console.WriteLine("Porcentaje poblacional");
+                    Class1.ExecuteExecersice3();
+                    break;
+            
+            }
+
+
+               
+        
+        }
+
+            
         }
     }
-}
