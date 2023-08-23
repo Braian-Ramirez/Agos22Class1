9e9831a22979aeaa3869512096e96ca83b0701ba
Braian-Ramirez <braiannf.ramirezo@konradlorenz.edu.co>
2023-08-22 21:31:57 -0500

    primer caso
    promedio tres enteros

---
+++ Agos22Class1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Agos22Class1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
